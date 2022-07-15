76fc74f44f3debbf39031d1860b1abe98a47e58d
= <luke@lukezsmith.com>
2022-07-15 12:03:42 +0100

    removed comment

--- portal.sh
+++ portal.sh
@@ -2,10 +2,10 @@
 GIT='git --git-dir='$PWD'/.git'
 
 # date interface
-echo Enter date of commit in MM/DD/YYYY format:
+echo Enter date of commit in MM-DD-YYYY format:
 read commit_date
 # git commit interface
 echo Enter commit message:
 read msg
-GIT_AUTHOR_DATE=$commit_date GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE" $GIT commit -m "$msg"
-# GIT_AUTHOR_DATE='1 month ago' GIT_COMMITTER_DATE="$GIT_AUTHOR_DATE" $GIT log --pretty=fuller
+now=$(date +"%T")
+GIT_AUTHOR_DATE="$commit_date $now" GIT_COMMITTER_DATE=$GIT_AUTHOR_DATE $GIT commit -m "$msg"
